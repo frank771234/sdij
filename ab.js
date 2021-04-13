08d97068778937a73252f082b9b8b023979b4403
frank771234 <frank771234@gmail.com>
2021-04-13 11:43:40 +0900

    upload files

---
+++ ab.js
@@ -0,0 +1,107 @@
+function changecolor(){
+    var x=document.getElementsByClassName('a')[0];
+    var y=document.getElementsByClassName('b')[0];
+    var z=document.getElementsByClassName('c')[0];
+    var w=document.getElementsByClassName('d')[0];
+    var s=document.getElementsByClassName('e')[0];
+    x.style.background="grey";
+    y.style.background="blue";
+    z.style.background="blue";
+    w.style.background="blue";
+    s.style.background="blue";
+    x.style.opacity=0.85;
+    y.style.opacity=1;
+    z.style.opacity=1;
+    w.style.opacity=1;
+    s.style.opacity=1;
+}
+
+function change(){
+    var x=document.getElementsByClassName('a')[0];
+    var y=document.getElementsByClassName('b')[0];
+    var z=document.getElementsByClassName('c')[0];
+    var w=document.getElementsByClassName('d')[0];
+    var s=document.getElementsByClassName('e')[0];
+    x.style.background="blue";
+    y.style.background="grey";
+    z.style.background="blue";
+    w.style.background="blue";
+    s.style.background="blue";
+    x.style.opacity=1;
+    y.style.opacity=0.85;
+    z.style.opacity=1;
+    w.style.opacity=1;
+    s.style.opacity=1;
+}
+function change1(){
+    var x=document.getElementsByClassName('a')[0];
+    var y=document.getElementsByClassName('b')[0];
+    var z=document.getElementsByClassName('c')[0];
+    var w=document.getElementsByClassName('d')[0];
+    var s=document.getElementsByClassName('e')[0];
+    x.style.background="blue";
+    y.style.background="blue";
+    z.style.background="grey";
+    w.style.background="blue";
+    s.style.background="blue";
+    x.style.opacity=1;
+    y.style.opacity=1;
+    z.style.opacity=0.85;
+    w.style.opacity=1;
+    s.style.opacity=1;
+}
+
+function change2(){
+    var x=document.getElementsByClassName('a')[0];
+    var y=document.getElementsByClassName('b')[0];
+    var z=document.getElementsByClassName('c')[0];
+    var w=document.getElementsByClassName('d')[0];
+    var s=document.getElementsByClassName('e')[0];
+    x.style.background="blue";
+    y.style.background="blue";
+    z.style.background="blue";
+    w.style.background="grey";
+    s.style.background="blue";
+    x.style.opacity=1;
+    y.style.opacity=1;
+    z.style.opacity=1;
+    w.style.opacity=0.85;
+    s.style.opacity=1;
+}
+function change3(){
+    var x=document.getElementsByClassName('a')[0];
+    var y=document.getElementsByClassName('b')[0];
+    var z=document.getElementsByClassName('c')[0];
+    var w=document.getElementsByClassName('d')[0];
+    var s=document.getElementsByClassName('e')[0];
+    x.style.background="blue";
+    y.style.background="blue";
+    z.style.background="blue";
+    w.style.background="blue";
+    s.style.background="grey";
+
+    x.style.opacity=1;
+    y.style.opacity=1;
+    z.style.opacity=1;
+    w.style.opacity=1;
+    s.style.opacity=1;
+}
+
+
+function changecolor1(){
+    var x=document.getElementsByClassName('a')[0];
+    var y=document.getElementsByClassName('b')[0];
+    var z=document.getElementsByClassName('c')[0];
+    var w=document.getElementsByClassName('d')[0];
+    var s=document.getElementsByClassName('e')[0];
+    x.style.background="blue";
+    y.style.background="blue";
+    z.style.background="blue";
+    w.style.background="blue";
+    s.style.background="blue";
+    x.style.opacity=1;
+    y.style.opacity=1;
+    z.style.opacity=1;
+    w.style.opacity=1;
+    s.style.opacity=1;
+}
